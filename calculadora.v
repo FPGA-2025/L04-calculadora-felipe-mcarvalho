76abf0bfd38c0ecb87e48a075cb8a8cdef652bba
Felipe Carvalho <felipemarinho92020@gmail.com>
2025-04-14 12:56:13 -0300

    Adicionando tarefa

--- calculadora.v
+++ calculadora.v
@@ -1,10 +1,17 @@
 module calculadora(
-  input   [7:0] entrada_A,
-  input   [7:0] entrada_B,
-  input   [2:0] codigo,
-  output  [7:0] saida
+    input [7:0] entrada_A,
+    input [7:0] entrada_B,
+    input [2:0] codigo,
+    output [7:0] saida
 );
 
-// insira seu código aqui
+  // insira seu código aqui
+  assign saida = (codigo == 3'b000) ? 8'b00000000 :
+                 (codigo == 3'b001) ? (entrada_A & entrada_A) :
+                 (codigo == 3'b010) ? (entrada_B & entrada_B) :
+                 (codigo == 3'b011) ? (entrada_A + entrada_B) :
+                 (codigo == 3'b100) ? (entrada_A - entrada_B) :
+                  entrada_A;
 
 endmodule
+
